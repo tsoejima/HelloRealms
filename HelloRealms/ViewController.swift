5c512bf1cb6bc1d4b05d0d32c7fbe06863e4171e
tsoejima <t.soejimadeveloper@gmail.com>
2021-01-24 09:00:36 +0900

    Initial Commit

---
+++ HelloRealms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloRealms
+//
+//  Created by 副島拓哉 on 2021/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
